9bcdf3edd09f651ec21bb207378ba1e7d719704a
talum <tracidini@gmail.com>
2015-12-22 17:37:04 -0500

    Changes margins

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 $(document).on("ready", function(){
   inspirationListener();
-
+  contactListener();
 })
 
 function inspirationListener(){
   $(".card").click(function(){
     $(this).children().toggle();
+  })
+}
+
+function contactListener(){
+  $(".more").hover(function(){
+    $(".contact").fadeIn("slow");
   })
 }
